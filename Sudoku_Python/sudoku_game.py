26ec8f4ff45ceece363f728f9b74460d0df37ae7
Briana Smith <thebrianasmith@gmail.com>
2020-02-23 20:25:11 -0700

    Update Cell fill and solution grid in game

--- Sudoku_Python/sudoku_game.py
+++ Sudoku_Python/sudoku_game.py
@@ -1,11 +1,13 @@
 import pygame
-from pygame.locals import *
+import copy
+from pygame.locals import Color, Rect
+from pygame.constants import (MOUSEBUTTONDOWN, KEYDOWN, K_BACKSPACE, QUIT)
 from sudoku import SudokuGrid
 
-SCREENRECT = Rect(0, 0, 640, 480)
-COLOR_INACTIVE = pygame.Color(223, 97, 97)
-COLOR_ACTIVE = pygame.Color(0, 0, 255)
-COLOR_LOCKED = pygame.Color(0, 0, 0)
+SCREENRECT = Rect(0, 0, 640, 640)
+COLOR_INACTIVE = Color(223, 97, 97)
+COLOR_ACTIVE = Color(0, 0, 255)
+COLOR_LOCKED = Color(0, 0, 0)
 
 pygame.font.init()
 font_path = pygame.font.match_font('dejavusansextralight')
@@ -13,22 +15,25 @@ player_font = pygame.font.Font(font_path, 20)
 player_font.set_bold(True)
 locked_font = pygame.font.Font(font_path, 20)
 locked_font.set_bold(True)
+cell_font = pygame.font.Font(font_path, 20)
 
-# textinput = TextInput('hello', 'dejavusansextralight')
 
 class Cell(pygame.sprite.Sprite):
     def __init__(self, x, y, w, h, row, col, text='', locked=False):
+        
         self.rect = pygame.Rect(x, y, w, h)
+        self.cell_height = h
+        self.cell_width = w
         self.text = text
         self.color = COLOR_LOCKED
-        self.txt_surface = player_font.render(text, True, self.color)
+        self.txt_surface = cell_font.render(text, True, self.color)
         self.active = False
         self.row = row
         self.col = col
         self.width = 1
 
     def handle_event(self, event):
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == MOUSEBUTTONDOWN:
             # If the user clicked on the rect.
             if self.rect.collidepoint(event.pos):
                 self.active = not self.active
@@ -38,27 +43,32 @@ class Cell(pygame.sprite.Sprite):
             self.color = COLOR_INACTIVE if self.active else COLOR_LOCKED
             self.width = 4 if self.active else 1
 
-        if event.type == pygame.KEYDOWN:
+        if event.type == KEYDOWN:
             if self.active:
-                if event.key == pygame.K_BACKSPACE:
+                if event.key == K_BACKSPACE:
                     self.text = self.text[:-1]
-                else:
+                elif event.key in range(49, 58):
+                    # Only allow keys from 1-9
                     self.text += event.unicode
 
                 if len(self.text) > 1:
+                    # Only allow one entry at a time
                     self.text = self.text[:1]
+
                 # Re-render the text.
                 sudoku_grid.fill_cell(self.row + 1, self.col + 1, self.text)
-                self.txt_surface = player_font.render(self.text, True, self.color)
+                self.txt_surface = cell_font.render(
+                    self.text, True, self.color)
 
     def draw(self, screen):
         # Blit the text. Use x and y variance to center text!
-        # center_x = self.rect.x  # Left
-        # center_y = (self.rect.y)  # Top
-        screen.blit(self.txt_surface, (self.rect.x + 5, self.rect.y))
-        
+        text_x = (self.cell_width - self.txt_surface.get_width()) / 2
+        text_y = (self.cell_height - cell_font.get_linesize()) / 2
+        screen.blit(self.txt_surface, (self.rect.x + text_x, self.rect.y + text_y))
+
         # Blit the rect.
         pygame.draw.rect(screen, self.color, self.rect, self.width)
+
 
 class LockedCell(Cell):
     def __init__(self, x, y, w, h, row, col, text):
@@ -72,25 +82,46 @@ class LockedCell(Cell):
 
 sudoku_grid = SudokuGrid(9, 0)
 sudoku_grid.enter_puzzle('easy_puzzle.txt')
-x, y = 0, 0  # coordinates of starting corner will be this
 
-cells = []
-grid_cells = []
+solution_grid = SudokuGrid(9, 0)
+solution_grid.enter_puzzle(sudoku_grid.solution_file)
 
-for row in range(sudoku_grid.size):
-    x = 0
-    for col in range(sudoku_grid.size):
-        val = sudoku_grid.get_cell(row, col)
-        if val == 0:
-            cells.append(Cell(x, y, 25, 25, row, col, text=''))
-        else:
-            cells.append(LockedCell(x, y, 25, 25, row, col, text=str(val)))
-        if (x == 0 or x % 75 == 0) and (y == 0 or y % 75 == 0):
-            grid_cells.append(Rect(x, y, 75, 75))
-        x += 25
-    y += 25
 
-main_grid = Rect(0, 0, 25*9, 25*9)
+def fill_cell(sudoku_grid, cells, x, y, row, col, cell_size):
+    val = sudoku_grid.get_cell(row, col)
+    if val == 0:
+        cells.append(Cell(x, y, cell_size, cell_size, row, col, text=''))
+    else:
+        cells.append(LockedCell(x, y, cell_size, cell_size, row, col, text=str(val)))
+
+def better_fill(sudoku_grid, grid_size, starting_corner):
+    cell_size = int(grid_size / sudoku_grid.size) # 225 / 9 = 25
+    inner_box_size = int((sudoku_grid.size / 3) * cell_size) # 9 /3 = 3 * 25 = 75
+
+    cells = []
+    grid_cells = []
+
+    start_x, start_y = starting_corner
+    x, y = copy.copy(starting_corner)
+    
+    for row in range(sudoku_grid.size):
+        x = start_x
+        for col in range(sudoku_grid.size):
+            fill_cell(sudoku_grid, cells, x, y, row, col, cell_size)
+
+            # If x is at starting coor, or if its coordinate - offset is divisible by inner_box_size
+            if (x == start_x or (x - start_x) % inner_box_size == 0) and \
+                (y == start_y or (y - start_y) % inner_box_size == 0):
+                grid_cells.append(Rect(x, y, inner_box_size, inner_box_size))
+            x += cell_size
+        y += cell_size
+
+    return cells, grid_cells
+
+grid_size = 400
+grid_coor = (100, 25)
+cells, grid_cells = better_fill(sudoku_grid, grid_size, grid_coor)
+
 
 def main():
     pygame.init()
@@ -109,6 +140,7 @@ def main():
         for event in events:
             if event.type == QUIT:
                 running = False
+
             for cell in cells:
                 cell.handle_event(event)
 
@@ -122,5 +154,6 @@ def main():
 
     print(sudoku_grid.print_grid())
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
